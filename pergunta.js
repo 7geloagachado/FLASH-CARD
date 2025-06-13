59d89d884d68c3889b51eb152bd977e01ed97bc6
7geloagachado <jean.lucas.silva06@escola.pr.gov.br>
2025-06-13 08:24:01 -0300

    code

---
+++ pergunta.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Brasil?',
+    'A capital do brasil e Brasilia'
+)
+
+criaCartao(
+    'Futebol',
+    'Quantas champions tem o Real Madrid?',
+    'Ele tem 15'
+)
+
+criaCartao(
+    'UFC ',
+    'Quantas vitorias o khabib Nurmagomedov tem?',
+    ' 29 sem perder nenhuma.(RAI)'
+)
